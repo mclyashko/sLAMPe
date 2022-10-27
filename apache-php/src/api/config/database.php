6cc23d87437a8c9f37b921fe92329afc7388ad74
Victor Lyashenko <kenuay007@gmail.com>
2022-10-27 12:48:50 +0300

    database config

--- apache-php/src/api/config/database.php
+++ apache-php/src/api/config/database.php
@@ -1,1 +1,20 @@
 <?php
+
+class Database
+{
+    // Данные базы данных
+    private string $host = 'mysql';
+    private string $db_name = 'appDB';
+    private string $username = 'user';
+    private string $password = 'password';
+    private ?mysqli $connection = null;
+
+    public function getConnection(): mysqli
+    {
+        if ($this->connection == null) {
+            $this->connection = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        }
+
+        return $this->connection;
+    }
+}
